d6b4b011e75c55e2facede6e78ce23bdd94e454e
Dinesh23040 <dinesh.cloudone@gmail.com>
2024-07-27 19:52:05 +0530

    first commit
    Signed-off-by: Dinesh23040 <dinesh.cloudone@gmail.com>

---
+++ src/test/java/testrunners/AmazonTest.java
@@ -0,0 +1,19 @@
+package testrunners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"src/test/resources/AppFeatures/Search.feature"},
+				glue = {"stepdefinitions"},
+		//glue = {"stepdefinitions", "MyHooks"}, //stepdefinitions and MyHooks are packages
+		//tags = "@Smoke or @Regression",
+		plugin = {"pretty"},
+		monochrome = true
+		)
+public class AmazonTest {
+
+}
